91db55f78bfb94f5aa68ee6bf06471bec082ac8f
Dan G <dang@delete.run>
2022-04-29 18:13:19 -0700

    plasma

--- configuration.nix
+++ configuration.nix
@@ -55,12 +55,12 @@
     # services.xrdp.defaultWindowManager = "xfce4-session";
     # services.xrdp.enable = true;
 
-    # displayManager.gdm.enable = true;
+    displayManager.gdm.enable = true;
     # displayManager.lightdm.enable = true;
-    displayManager.sddm.enable  = true;
-    desktopManager.xfce.enable = true;
+    # displayManager.sddm.enable  = true;
+    # desktopManager.xfce.enable = true;
     desktopManager.plasma5.enable = true;
-    desktopManager.gnome.enable = true;
+    # desktopManager.gnome.enable = true;
   };
   #services.xrdp.defaultWindowManager = "gnome-shell";
 
