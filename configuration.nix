2d03fb3b5cdd538d2f943ef588f3b14dd4636edf
Dan G <dang@delete.run>
2022-04-29 18:05:59 -0700

    gui and network fix

--- configuration.nix
+++ configuration.nix
@@ -12,10 +12,11 @@
 
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
+
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  networking.hostName = "nix-pad"; # Define your hostname.
+  networking.hostName = "nixipad"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
@@ -26,8 +27,8 @@
   # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
+  networking.interfaces.wlan0.useDHCP = true;
   networking.networkmanager.enable = true;
-  #networking.interfaces.wlan0.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -41,47 +42,41 @@
   };
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  services.xserver.desktopManager.plasma5.enable = true;
-
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-  #   desktopManager.gnome.enable = true;
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    # libinput.enable = true;
+
+    # Configure keymap in X11
+    # layout = "us";
+    # xkbOptions = "eurosign:e";
+
+    # services.xrdp.defaultWindowManager = "xfce4-session";
+    # services.xrdp.enable = true;
+
+    # displayManager.gdm.enable = true;
+    # displayManager.lightdm.enable = true;
+    displayManager.sddm.enable  = true;
+    desktopManager.xfce.enable = true;
+    desktopManager.plasma5.enable = true;
+    desktopManager.gnome.enable = true;
   };
+  services.xrdp.defaultWindowManager = "gnome-shell";
+
   # environment.gnome.excludePackages = [
   #   pkgs.gnome.cheese pkgs.gnome-photos pkgs.gnome.gnome-music
   #   pkgs.gnome.gnome-terminal pkgs.gnome.gedit pkgs.epiphany pkgs.evince
   #   pkgs.gnome.gnome-characters pkgs.gnome.totem pkgs.gnome.tali
   #   pkgs.gnome.iagno pkgs.gnome.hitori pkgs.gnome.atomix pkgs.gnome-tour
   # ];
-  # services.xrdp.defaultWindowManager = "gnome-shell";
-
-#   services.xserver = {
-#     enable = true;
-#     displayManager.lightdm.enable = true;
-#     desktopManager.xfce.enable = true;
-#   };
-#   services.xrdp.defaultWindowManager = "xfce4-session";
-#   services.xrdp.enable = true;
-
-
-
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
+  
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
   # Enable sound.
   # sound.enable = true;
   # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  #services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.me = {
